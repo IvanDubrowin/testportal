da4bdaba25a8aaa0f774c4ca8c5c16c4b4773523
Unknown <ivandubrovin2@mail.ru>
2018-09-07 18:48:18 +0300

    3

--- portal/models.py
+++ portal/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-
 class Course(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.TextField()
@@ -10,6 +9,56 @@ class Course(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class UserCurrentCourse(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    stat_true_ans = models.IntegerField(default=0)
+    count = models.IntegerField(default=0)
+    del_ans = models.IntegerField(default=0)
+
+    ans = {}
+
+    def get_question(self):
+        query = Question.objects.filter(course = self.course)
+        questions = list(query)[self.del_ans:(self.count + 1)]
+        return questions
+
+    def true_answer(self):
+        question = list(
+            Question.objects.filter(course=self.course).values('true_ans')
+        )
+        try:
+            self.ans.update(question[self.count])
+        except IndexError:
+            pass
+        return self.ans
+
+    def save_answer(self, form):
+        self.count += 1
+        self.del_ans += 1
+        self.save()
+        user_answer = int(form.data['field'])
+        true_ans = int(self.ans['true_ans'])
+
+        if user_answer == true_ans:
+            self.stat_true_ans += 1
+            self.save()
+
+    def end_learn(self):
+        self.ans.clear()
+        self.count = 0
+        self.del_ans = 0
+        stat = UserCourseStat(
+                        id_user=self.user,
+                        id_course=self.course,
+                        correct_answers=self.stat_true_ans,
+                        total_answers=len(Question.objects.filter(course=self.course))
+                    )
+        stat.save()
+        self.stat_true_ans = 0
+        self.save()
 
 
 class Question(models.Model):
@@ -26,13 +75,12 @@ class Question(models.Model):
         return self.text_question
 
 
-
 class UserCourseStat(models.Model):
     id_user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_course = models.ForeignKey(Course, on_delete=models.CASCADE)
     correct_answers = models.IntegerField()
     total_answers = models.IntegerField()
-    data = models.DateTimeField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return 'Пользователь: %s, %s'  % (self.id_user, self.id_course)
--- portal/views.py
+++ portal/views.py
@@ -6,12 +6,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Question, Course, UserCourseStat
+from .models import Question, Course, UserCourseStat, UserCurrentCourse
 from .forms import QuestionChoices
 
 
 class RegisterFormView(FormView):
-    
+
     form_class = UserCreationForm
     success_url = "accounts/login/"
     template_name = "registration.html"
@@ -28,7 +28,7 @@ class StatisticView(View, LoginRequiredMixin):
     @method_decorator(login_required)
     def get(self, request):
         user = User.objects.get(username=request.user)
-        stat = UserCourseStat.objects.filter(id_user=user)
+        stat = UserCourseStat.objects.filter(id_user=user).order_by('-date')
         args = {"stat": stat }
         return render(request, self.template_name, args)
 
@@ -36,53 +36,32 @@ class StatisticView(View, LoginRequiredMixin):
 class CourseView(View, LoginRequiredMixin):
 
     template_name = 'course.html'
-    stat_true_ans = 0
-    count = 0
-    del_ans = 0
-    ans = {}
 
     @method_decorator(login_required)
     def get(self, request, pk):
-        query = Question.objects.filter(course = pk)
-        form = QuestionChoices()
-        questions = list(query)[CourseView.del_ans:(CourseView.count + 1)]
-        args = {"questions" : questions, "form" : form}
+        try:
+            user_learning = UserCurrentCourse.objects.get(user=request.user, course=pk)
+        except UserCurrentCourse.DoesNotExist:
+            user_learning = UserCurrentCourse(
+                        user = request.user,
+                        course = Course.objects.get(id=pk)
+                    )
+            user_learning.save()
 
+        form = QuestionChoices()
+        args = {"questions" : user_learning.get_question(), "form" : form}
         return render(request, self.template_name, args)
 
     @method_decorator(login_required)
     def post(self, request, pk):
-        question = list(
-        Question.objects.filter(course=pk).values('true_ans')
-        )
-        try:
-            CourseView.ans.update(question[CourseView.count])
-        except IndexError:
-            pass
+        user_learning = UserCurrentCourse.objects.get(user=request.user, course=pk)
+        user_learning.true_answer()
         form = QuestionChoices(self.request.POST)
 
         if form.is_valid():
-            user = User.objects.get(username=request.user)
-            course = Course.objects.get(id=pk)
-            CourseView.count += 1
-            CourseView.del_ans += 1
-            user_answer = int(form.data['field'])
-            true_ans = int(CourseView.ans['true_ans'])
-
-            if user_answer == true_ans:
-                CourseView.stat_true_ans += 1
-
-            if CourseView.count == len(question):
-                CourseView.ans.clear()
-                CourseView.count = 0
-                CourseView.del_ans = 0
-                stat = UserCourseStat(
-                                    id_user=user,
-                                    id_course=course,
-                                    correct_answers=CourseView.stat_true_ans,
-                                    total_answers=len(question))
-                stat.save()
-                CourseView.stat_true_ans = 0
+            user_learning.save_answer(form)
+            if user_learning.count == len(Question.objects.filter(course=user_learning.course)):
+                user_learning.end_learn()
                 return redirect('portal:statistic')
 
         return redirect('portal:course', pk)
